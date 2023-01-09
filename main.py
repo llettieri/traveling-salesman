78ee658ce8a5f6f77be213f807073baaef15e443
lena-bruggmann <lena.bruggmann@bluewin.ch>
2023-01-09 08:53:38 +0100

    Fluglinie brechnen

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import math
 from itertools import permutations
 
+
+def flight_line(x1, y1, x2, y2):
+    x = x2-x1
+    y = y2-y1
+    result = math.sqrt(math.pow(x, 2) + math.pow(y, 2))
+    return result
 
 def traveling_salesman(cities, distances):
     # Generate all possible permutations of the cities
@@ -15,25 +22,24 @@ def traveling_salesman(cities, distances):
         for i in range(len(permutation) - 1):
             city_a = permutation[i]
             city_b = permutation[i + 1]
-            distance += distances[city_a][city_b]
+            distance += flight_line(distances[city_a][0], distances[city_a][1], distances[city_b][0], distances[city_b][1])
 
         # If the total distance for this permutation is less than the current minimum distance, set the minimum
         # distance to this distance
         if distance < min_distance:
             min_distance = distance
             min_path = permutation
-
     # Return the minimum distance
     return min_distance, list(min_path)
-
-
 # Example usage
-cities = ['A', 'B', 'C', 'D']
+cities = ['Barcelona', 'Amsterdam', 'New York']
+
+
 distances = {
-    'A': {'B': 70, 'C': 65, 'D': 25},
-    'B': {'A': 70, 'C': 30, 'D': 100},
-    'C': {'A': 65, 'B': 30, 'D': 20},
-    'D': {'A': 25, 'B': 100, 'C': 20},
+    'Barcelona': [41.3927755,2.0701491],
+    'Amsterdam': [52.3547498,4.8339208],
+    'New York': [40.6976701,-74.2598663]
 }
+
 
 print(traveling_salesman(cities, distances))  # Outputs: 30
